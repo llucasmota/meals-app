8403f5d1509f6d7c1fe234509fb587fe9f3d676e
llucasmota <lucas.o.mota@gmail.com>
2023-01-16 21:58:34 -0300

    feat(Meal, dummy_data): criando classe Meal e novo mock para atender a classe Meal

---
+++ lib/models/meal.dart
@@ -0,0 +1,38 @@
+enum Complexity {
+  simple,
+  medium,
+  difficult,
+}
+
+enum Cost { cheap, fair, expensive }
+
+class Meal {
+  final String id;
+  final String title;
+  final List<String> categories;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final Complexity complexity;
+  final Cost cost;
+
+  const Meal(
+      {required this.id,
+      required this.categories,
+      required this.title,
+      required this.cost,
+      required this.complexity,
+      required this.imageUrl,
+      required this.ingredients,
+      required this.steps,
+      required this.isGlutenFree,
+      required this.isLactoseFree,
+      required this.isVegan,
+      required this.isVegetarian,
+      required this.duration});
+}
