5cfc2ac1d2ced4ccf4f044ba0520ee65f1718744
llucasmota <lucas.o.mota@gmail.com>
2023-01-19 21:44:11 -0300

    feat(meal_detail_screen): criação de nova screen para detalhamento das refeições

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -6,16 +6,80 @@ import 'package:meals/models/meal.dart';
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({super.key});
 
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  Widget _createSection(Widget child) {
+    return Container(
+        width: 350,
+        height: 350,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        child: child);
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: Center(
-        child: Text(meal.ingredients.toString()),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(meal.imageUrl, fit: BoxFit.cover),
+              ),
+              _createSectionTitle(context, 'Ingredientes'),
+              _createSection(
+                ListView.builder(
+                    itemCount: meal.ingredients.length,
+                    itemBuilder: (_, index) {
+                      return Card(
+                        elevation: 4,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5.0, horizontal: 10.0),
+                          child: Text(meal.ingredients[index]),
+                        ),
+                        color: Theme.of(context).colorScheme.secondary,
+                      );
+                    }),
+              ),
+              _createSectionTitle(context, 'Passos'),
+              _createSection(ListView.builder(
+                  itemCount: meal.steps.length,
+                  itemBuilder: (_, index) {
+                    return Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Text('${index + 1}'),
+                          ),
+                          title: Text(meal.steps[index]),
+                        ),
+                        Divider(color: Colors.blueGrey),
+                      ],
+                    );
+                  }))
+            ],
+          ),
+        ));
   }
 }
