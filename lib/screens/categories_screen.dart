8e27b47e9dfa309e568101ace9bc8f976006abe6
llucasmota <lucas.o.mota@gmail.com>
2023-01-19 21:43:04 -0300

    feat(categories_screen): removendo Scaffold

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,33 +5,26 @@ import '../components/category_item.dart';
 
 import 'package:flutter/material.dart';
 
-class CategoriesScreens extends StatelessWidget {
-  const CategoriesScreens({super.key});
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Vamos cozinhar?"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: GridView(
-          /**
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      /**
            * Sliver  é uma área com scroll no flutter
            * GridDelegateWithMaxCrossAxisExtent
            */
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20.0,
-            mainAxisSpacing: 20.0,
-          ),
-          children: DUMMY_CATEGORIES.map((category) {
-            return CategoryItem(category);
-          }).toList(),
-        ),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
       ),
+      children: DUMMY_CATEGORIES.map((category) {
+        return CategoryItem(category);
+      }).toList(),
     );
   }
 }
