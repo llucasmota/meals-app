6ca10b2188ca2479409b0d91465cb6ca7b3d238e
llucasmota <lucas.o.mota@gmail.com>
2023-01-21 19:45:05 -0300

    feat(): criando código reutilizável para criar seções

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -35,51 +35,61 @@ class MealDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(meal.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Image.network(meal.imageUrl, fit: BoxFit.cover),
-              ),
-              _createSectionTitle(context, 'Ingredientes'),
-              _createSection(
-                ListView.builder(
-                    itemCount: meal.ingredients.length,
-                    itemBuilder: (_, index) {
-                      return Card(
-                        elevation: 4,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 5.0, horizontal: 10.0),
-                          child: Text(meal.ingredients[index]),
-                        ),
-                        color: Theme.of(context).colorScheme.secondary,
-                      );
-                    }),
-              ),
-              _createSectionTitle(context, 'Passos'),
-              _createSection(ListView.builder(
-                  itemCount: meal.steps.length,
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(meal.imageUrl, fit: BoxFit.cover),
+            ),
+            _createSectionTitle(context, 'Ingredientes'),
+            _createSection(
+              ListView.builder(
+                  itemCount: meal.ingredients.length,
                   itemBuilder: (_, index) {
-                    return Column(
-                      children: <Widget>[
-                        ListTile(
-                          leading: CircleAvatar(
-                            child: Text('${index + 1}'),
-                          ),
-                          title: Text(meal.steps[index]),
-                        ),
-                        Divider(color: Colors.blueGrey),
-                      ],
+                    return Card(
+                      elevation: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 10.0),
+                        child: Text(meal.ingredients[index]),
+                      ),
+                      color: Theme.of(context).colorScheme.secondary,
                     );
-                  }))
-            ],
-          ),
-        ));
+                  }),
+            ),
+            _createSectionTitle(context, 'Passos'),
+            _createSection(
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (_, index) {
+                  return Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(meal.steps[index]),
+                      ),
+                      Divider(color: Colors.blueGrey),
+                    ],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.star, color: Theme.of(context).colorScheme.primary),
+        onPressed: () {
+          Navigator.of(context).pop(meal.title);
+        },
+      ),
+    );
   }
 }
